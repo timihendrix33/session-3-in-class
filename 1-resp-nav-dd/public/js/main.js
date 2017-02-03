759bad6adb559a60541472894e7e1256beeabde6
DannyBoyNYC <daniel.deverell@gmail.com>
2017-02-03 17:14:51 -0500

    initial commit

---
+++ 1-resp-nav-dd/public/js/main.js
@@ -0,0 +1,56 @@
+const nav = document.getElementById('main');
+const navLinks = document.getElementById('nav-links');
+const markup =
+  `${navItems.map(listItem => `<li><a href="${listItem.link}">${listItem.label}</a></li>`).join('')}`;
+navLinks.innerHTML = markup;
+
+
+
+let topOfNav = nav.offsetTop;
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
+}
+window.addEventListener('scroll', fixNav);
+
+
+
+const siteWrap = document.querySelector('.site-wrap');
+window.onload = function () {
+  window.location.hash = '#watchlist'
+}
+
+window.onhashchange = function (e) {
+  let newloc = window.location.hash;
+  let newContent = navItems.filter(navItem => navItem.link == newloc);
+  siteWrap.innerHTML = `
+  <h1>${newContent[0].label}</h1>
+  <h2>${newContent[0].header}</h2>
+  <p>${newContent[0].content}</p>
+  `;
+  showMenu(e);
+}
+
+
+const logo = document.querySelector('.logo');
+if (document.documentElement.clientWidth <= 740) {
+  logo.addEventListener('click', showMenu);
+}
+
+function showMenu(e){
+  document.body.classList.toggle('show');
+  e.preventDefault();
+}
+
+
+
+
+
+
+
+
